a8bebe7a9cfaac63d933c27bac4fbb78c8d832ed
Andrey Onipchenko <andreysk8er@ukr.net>
2021-03-16 22:13:05 +0200

    add data

--- src/utils/dataBase.js
+++ src/utils/dataBase.js
@@ -1,11 +1,16 @@
 const Sequelize = require("sequelize");
 
-const SCHEMA_NAME = "post";
-const USER_NAME = "andrey";
-const PASSWORD = "123q123q";
+// const SCHEMA_NAME = "post";
+// const USER_NAME = "andrey";
+// const PASSWORD = "123q123q";
+
+const SCHEMA_NAME = "sp_nod";
+const USER_NAME = "sp_nod";
+const PASSWORD = "m15N89BC65ng'";
 
 const sequelize = new Sequelize(SCHEMA_NAME, USER_NAME, PASSWORD, {
-  host: "localhost",
+  // host: "localhost",
+  host: "192.168.213.250",
   dialect: "mysql",
 });
 
